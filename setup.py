3dcdbbb54308abf4376b1061b6f9611656a18d84
s9w <swerhausen@googlemail.com>
2014-11-09 23:31:25 +0100

    preparing for pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 # coding=utf-8
-from setuptools import setup
-setup(name='pretex', packages=['pretex'])
+from distutils.core import setup
+setup(name='preTeX',
+      version="0.1.0",
+      packages=['pretex'],
+      author='Sebastian Werhausen',
+      author_email="swerhausen@gmail.com",
+      description="LaTeX preprocessor to simplify math typesetting",)
