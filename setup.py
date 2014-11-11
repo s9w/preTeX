a31707d1673a9e86f037ad45e0e4b2a42f2d726b
s9w <swerhausen@googlemail.com>
2014-11-12 00:48:13 +0100

    update pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='preTeX',
-      version="0.2.1",
+      version="0.3.0",
       packages=['pretex'],
       entry_points={
            'console_scripts': [
